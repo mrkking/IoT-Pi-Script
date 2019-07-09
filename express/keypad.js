290bd7ecc5e812b982f7ef8c115a05fa46c2273d
Kory King <kking9351@gmail.com>
2019-07-09 14:22:28 -0400

    second-commit

--- express/keypad.js
+++ express/keypad.js
@@ -3,7 +3,7 @@ const app = express();
 const {accessKeys} = require('../db/db_handler');
 
 app.get('/:pin', (req, res) => {
-  const pin = req.params.pin;
+  const pin = parseInt(req.params.pin);
   console.log(typeof pin);
   console.log(accessKeys.getKeyByPin(pin));
   res.send({});
