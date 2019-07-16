f34fdd2147e680a6f57af61694bb44c41651e295
Kory King <kking9351@gmail.com>
2019-07-16 11:46:31 -0400

    switch

--- keypad_handler.js
+++ keypad_handler.js
@@ -1,14 +1,22 @@
 const {spawn} = require('child_process');
 const config = require(__dirname+'/config');
 
-const cmd = spawn('python', [__dirname + '/gate_py_scripts/keypad.py']);
-cmd.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
-cmd.stderr.on('data', (data) => {
-  console.log('Failed to initialize keypad');
-  console.log(`stderr: ${data}`);
-});
-cmd.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+const spawn_keypad_process = () => {
+  const cmd =  spawn('python', [__dirname + '/gate_py_scripts/keypad.py']);
+  cmd.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+  cmd.stderr.on('data', (data) => {
+    console.log('Failed to initialize keypad');
+    console.log(`stderr: ${data}`);
+  });
+  cmd.on('close', (code) => {
+    if (code === 0) {
+      spawn_keypad_process();
+    }
+    console.log(`child process exited with code ${code}`);
+  });
+};
+
+spawn_keypad_process();
+
